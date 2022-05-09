86f2c783b299dd0a030df0632673a35eb93294c0
Carlos Roberto <crpu4a@gmail.com>
2022-05-09 20:30:55 -0300

    Criação do componente ImageUI

---
+++ src/components/ui/image/ImageBoxUI.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { ImageIcon } from "../../icons";
+
+export interface ImageUIProps {
+  title: string;
+  url: string;
+  radius?: string;
+}
+
+withDefaults(defineProps<ImageUIProps>(), {
+  title: "",
+  url: "",
+  radius: "0px",
+});
+</script>
+
+<template>
+  <div
+    class="image-ui__container"
+    :title="title"
+    :style="{ borderRadius: radius }"
+  >
+    <div
+      v-if="url"
+      class="image-ui__bg"
+      :style="{ backgroundImage: `url(${url})`, borderRadius: radius }"
+    ></div>
+
+    <ImageIcon class="image-ui__icon" />
+  </div>
+</template>
+
+<style scoped>
+.image-ui__container {
+  background: #dee4ed;
+  width: 100%;
+}
+.image-ui__image {
+  width: 100%;
+  z-index: 10;
+}
+.image-ui__bg {
+  z-index: 10;
+  display: flex;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  aspect-ratio: 1/1;
+  background-size: 100%;
+  background-position: center center;
+  transition: all 300ms ease-in-out;
+}
+
+.image-ui__bg:hover {
+  background-size: 110%;
+}
+
+.image-ui__default {
+  width: 100%;
+  display: flex;
+  aspect-ratio: 1/1;
+}
+
+.image-ui__icon {
+  width: 100%;
+  z-index: 5;
+  height: 25%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  fill: #9eadba;
+}
+</style>
---
+++ src/components/ui/image/ImageUI.stories.ts
@@ -0,0 +1,34 @@
+import ImageBoxUI from "./ImageBoxUI.vue";
+
+export default {
+  title: "UI / ImageBoxUI",
+  component: ImageBoxUI,
+  args: {
+    title: "Wooow, look this!",
+    url: "https://image.tmdb.org/t/p/w500/pe17f8VDfzbvbHSAKAlcORtBHmW.jpg",
+    radius: "20px",
+  },
+  argTypes: {
+    title: { type: "string" },
+    url: { type: "string" },
+  },
+  parameters: {
+    backgrounds: {
+      default: "light",
+    },
+  },
+};
+
+export const Component = (args: unknown) => ({
+  components: { ImageBoxUI },
+  setup() {
+    return {
+      args,
+    };
+  },
+  template: `
+    <div class="default--storybook w-350">
+      <ImageBoxUI v-bind="args" />
+    </div>
+  `,
+});
